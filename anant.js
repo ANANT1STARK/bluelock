52c5c1d8e6721b0216fea745c2f6aeba10b3f4af
ANANT_stark <106265494+ANANT1STARK@users.noreply.github.com>
2025-01-23 21:22:33 +0530

    Add files via upload

---
+++ anant.js
@@ -0,0 +1,9 @@
+function showPopup() {
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('popupOverlay').style.display = 'block';
+}
+
+function hidePopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('popupOverlay').style.display = 'none';
+}
